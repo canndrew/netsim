2266ccf9dab8bae46d16864d3d27406a1f0edd61
Povilas Balciunas <balciunas90@gmail.com>
2018-08-10 13:26:36 +0300

    feat/network: add spawn methods

---
+++ tests/network.rs
@@ -0,0 +1,28 @@
+#[macro_use]
+extern crate net_literals;
+extern crate netsim;
+extern crate tokio_core;
+extern crate futures;
+#[macro_use]
+extern crate unwrap;
+
+use netsim::{node, Ipv4Range, Network};
+use futures::sync::oneshot;
+use futures::Future;
+use tokio_core::reactor::Core;
+
+#[test]
+fn spawn_ipv4_tree() {
+    let mut evloop = unwrap!(Core::new());
+    let network = Network::new(&evloop.handle());
+
+    let (addr_tx, addr_rx) = oneshot::channel();
+
+    let node_recipe = node::ipv4::machine(|ip| {
+        unwrap!(addr_tx.send(ip));
+    });
+    let (spawn_complete, _ipv4_plug) = network.spawn_ipv4_tree(Ipv4Range::new(ipv4!("78.100.10.1"), 30), node_recipe);
+
+    let addr = unwrap!(evloop.run(spawn_complete.and_then(|()| addr_rx.map_err(|e| panic!(e)))));
+    assert_eq!(addr.octets()[0..3], [78, 100, 10]);
+}
