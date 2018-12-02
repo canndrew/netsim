39dea3cd456ba26773c313f0344ecd1e9a48ee27
Povilas Balciunas <balciunas90@gmail.com>
2018-12-02 10:54:07 +0200

    Extract some code
    and add more tests. Makes it easier to change the code.

--- src/device/ipv4/router.rs
+++ src/device/ipv4/router.rs
@@ -37,24 +37,22 @@ impl Ipv4RouterBuilder {
 
 /// Connects a bunch of Ipv4 networks and routes packets between them.
 pub struct Ipv4Router {
-    rxs: Vec<Ipv4Receiver>,
+    // the only reason it's optional, is to make the borrow checker happy
+    rxs: Option<Vec<Ipv4Receiver>>,
     txs: Vec<(Ipv4Sender, Vec<Ipv4Route>)>,
     ipv4_addr: Ipv4Addr,
 }
 
 impl Ipv4Router {
-    /// Create a new router with the given connections. You can also use `Ipv4RouterBuilder` to add
-    /// connections individually.
+    /// Create a new router with the given IP address and connections. You can also use
+    /// `Ipv4RouterBuilder` to add connections individually.
     pub fn new(ipv4_addr: Ipv4Addr, connections: Vec<(Ipv4Plug, Vec<Ipv4Route>)>) -> Ipv4Router {
-        let mut rxs = Vec::with_capacity(connections.len());
-        let mut txs = Vec::with_capacity(connections.len());
-        for (plug, routes) in connections {
-            let (tx, rx) = plug.split();
-            rxs.push(rx);
-            txs.push((tx, routes));
+        let (rxs, txs) = split_conn_plugs(connections);
+        Ipv4Router {
+            rxs: Some(rxs),
+            txs,
+            ipv4_addr,
         }
-
-        Ipv4Router { rxs, txs, ipv4_addr }
     }
 
     /// Create a new `Ipv4Router`, spawning it directly onto the tokio event loop.
@@ -66,6 +64,46 @@ impl Ipv4Router {
         let router_v4 = Ipv4Router::new(ipv4_addr, connections);
         handle.spawn(router_v4.infallible());
     }
+
+    /// Checks if given packet, is destined to the router itself.
+    fn is_packet_to_me(&self, packet: &Ipv4Packet) -> bool {
+        packet.dest_ip() == self.ipv4_addr
+    }
+
+    /// Find a plug for given packet by it's destination address and send the packet.
+    /// Returns true if packet was sent, false otherwise.
+    fn send_packet(&mut self, packet: Ipv4Packet) -> bool {
+        for &mut (ref mut tx, ref routes) in &mut self.txs {
+            for route in routes {
+                if route.destination().contains(packet.dest_ip()) {
+                    info!(
+                        "router {} routing packet on route {:?} {:?}",
+                        self.ipv4_addr, route, packet,
+                    );
+                    tx.unbounded_send(packet);
+                    return true;
+                }
+            }
+        }
+        info!(
+            "router {} dropping unroutable packet {:?}",
+            self.ipv4_addr, packet
+        );
+        false
+    }
+}
+
+fn split_conn_plugs(
+    connections: Vec<(Ipv4Plug, Vec<Ipv4Route>)>,
+) -> (Vec<Ipv4Receiver>, Vec<(Ipv4Sender, Vec<Ipv4Route>)>) {
+    let mut rxs = Vec::with_capacity(connections.len());
+    let mut txs = Vec::with_capacity(connections.len());
+    for (plug, routes) in connections {
+        let (tx, rx) = plug.split();
+        rxs.push(rx);
+        txs.push((tx, routes));
+    }
+    (rxs, txs)
 }
 
 impl Future for Ipv4Router {
@@ -74,34 +112,21 @@ impl Future for Ipv4Router {
 
     fn poll(&mut self) -> Result<Async<()>, Void> {
         let mut all_disconnected = true;
-        for rx in &mut self.rxs {
-            all_disconnected &= {
-                'next_packet: loop {
-                    match rx.poll().void_unwrap() {
-                        Async::NotReady => break false,
-                        Async::Ready(None) => break true,
-                        Async::Ready(Some(packet)) => {
-                            let dest_ip = packet.dest_ip();
-                            if dest_ip == self.ipv4_addr {
-                                continue;
-                            }
-
-                            for &mut (ref mut tx, ref routes) in &mut self.txs {
-                                for route in routes {
-                                    if route.destination().contains(dest_ip) {
-                                        info!("router {} routing packet on route {:?} {:?}", self.ipv4_addr, route, packet);
-                                        tx.unbounded_send(packet);
-                                        continue 'next_packet;
-                                    }
-                                }
-                            }
-
-                            info!("router {} dropping unroutable packet {:?}", self.ipv4_addr, packet);
-                        },
+        let mut rxs = unwrap!(self.rxs.take());
+        for rx in &mut rxs {
+            all_disconnected &= loop {
+                match rx.poll().void_unwrap() {
+                    Async::NotReady => break false,
+                    Async::Ready(None) => break true,
+                    Async::Ready(Some(packet)) => {
+                        if !self.is_packet_to_me(&packet) {
+                            let _ = self.send_packet(packet);
+                        }
                     }
                 }
             };
         }
+        self.rxs = Some(rxs);
 
         if all_disconnected {
             return Ok(Async::Ready(()));
@@ -111,3 +136,66 @@ impl Future for Ipv4Router {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod ipv4_router {
+        use super::*;
+
+        mod send_packet {
+            use super::*;
+
+            fn udp_packet_v4(src: SocketAddrV4, dst: SocketAddrV4) -> Ipv4Packet {
+                Ipv4Packet::new_from_fields(
+                    Ipv4Fields {
+                        source_ip: src.ip().clone(),
+                        dest_ip: dst.ip().clone(),
+                        ttl: 10,
+                    },
+                    &Ipv4Payload::Udp(UdpPacket::new_from_fields_v4(
+                        &UdpFields {
+                            source_port: src.port(),
+                            dest_port: dst.port(),
+                        },
+                        src.ip().clone(),
+                        dst.ip().clone(),
+                        &Bytes::new(),
+                    )),
+                )
+            }
+
+            #[test]
+            fn it_returns_false_when_packet_sender_is_not_found_for_packet_destination_ip() {
+                let mut router = Ipv4Router::new(ipv4!("192.168.1.1"), vec![]);
+                let packet =
+                    udp_packet_v4(addrv4!("192.168.1.100:5000"), addrv4!("192.168.1.200:6000"));
+
+                let sent = router.send_packet(packet);
+
+                assert!(!sent);
+            }
+
+            #[test]
+            fn it_sends_packet_to_the_channel_associated_with_packet_destination_address() {
+                let (plug1_a, mut plug1_b) = Ipv4Plug::new_pair();
+                let conns = vec![(
+                    plug1_a,
+                    vec![Ipv4Route::new(
+                        Ipv4Range::new(ipv4!("192.168.1.0"), 24),
+                        None,
+                    )],
+                )];
+                let mut router = Ipv4Router::new(ipv4!("192.168.1.1"), conns);
+                let packet =
+                    udp_packet_v4(addrv4!("192.168.1.100:5000"), addrv4!("192.168.1.200:6000"));
+
+                let sent = router.send_packet(packet.clone());
+
+                assert!(sent);
+                let received_packet = plug1_b.poll();
+                assert_eq!(received_packet, Ok(Async::Ready(Some(packet))));
+            }
+        }
+    }
+}
